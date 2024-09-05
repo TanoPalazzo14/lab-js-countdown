2f5811fcb5a02f3cbfd2f6ec11e3d4f7486c3748
Juan Palazzo <j_palazzo@hotmail.com>
2024-09-05 16:01:39 +0200

    lab finalizado

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
 const DURATION = 10; // 10 seconds
+
 let remainingTime = DURATION; // Countdown starting from 10
-let timer = null; // Variable to store the interval
-
-
+let timer = document.querySelector("#time") // Variable to store the interval
+timer.innerText = `${DURATION}`
+let toast = document.querySelector(".toast")
+toast.style.webkitAnimation = "fadein 0.5s, fadeout 0.9s 2.5s";
+toast.style.animation = `fadein 0.5s, fadeout 0.9s 2.5s`;
+let countdownOn = false
 
 // ITERATION 1: Add event listener to the start button
 
 // Your code goes here ...
-
+const startBtnNode = document.querySelector("#start-btn")
+startBtnNode.addEventListener("click", () => {
+  if (!countdownOn){
+    startCountdown()
+    countdownOn = true
+  }
+})
 
 
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
   console.log("startCountdown called!");
-
+  let finalCountdown = setInterval(() => {
+    remainingTime--;
+    timer.innerText = `${remainingTime}`
+    if (remainingTime<=0){
+      clearInterval(finalCountdown)
+      showToast()
+    }
+  },1000)
 
   // Your code goes here ...
 }
@@ -23,9 +40,12 @@ function startCountdown() {
 
 
 // ITERATION 3: Show Toast
-function showToast(message) {
-  console.log("showToast called!");
-
+function showToast() {
+  toast.style.visibility = `visible`
+  
+  setTimeout(() => {
+    toast.style.visibility = `hidden`
+  },3000)
   // Your code goes here ...
 
 
